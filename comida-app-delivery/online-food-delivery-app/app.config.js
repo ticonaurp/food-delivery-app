2e62fa489e2a6ce0acce86890202f0281719624a
Geraldinel2109 <202121052@urp.edu.pe>
2025-07-06 04:57:06 +0000

    implementacion de los criterios - Github Actions,  Dashboard de Expo con artefacto publicado

--- comida-app-delivery/online-food-delivery-app/app.config.js
+++ comida-app-delivery/online-food-delivery-app/app.config.js
@@ -5,6 +5,7 @@ export default {
     version: "1.0.0",
     orientation: "portrait",
     userInterfaceStyle: "light",
+    platforms: ["ios", "android", "web"],
     splash: {
       resizeMode: "contain",
       backgroundColor: "#ffffff",
@@ -22,16 +23,17 @@ export default {
     },
     web: {},
     extra: {
-
-        apiKey: "AIzaSyBkKHooC7L5oHPSJUEjCFO0nswTHzhIp60",
-  authDomain: "onlinedeliveryapp-19236.firebaseapp.com",
-  projectId: "onlinedeliveryapp-19236",
-  storageBucket: "onlinedeliveryapp-19236.appspot.com",
-  messagingSenderId: "314147140443",
-  appId: "1:314147140443:web:a1ecbe9f3879bdf091607e",
-
+      eas: {
+        projectId: "fcbeaba3-e974-47d0-a3c4-b51161aa9277"
+      },
+      apiKey: "AIzaSyBkKHooC7L5oHPSJUEjCFO0nswTHzhIp60",
+      authDomain: "onlinedeliveryapp-19236.firebaseapp.com",
+      projectId: "onlinedeliveryapp-19236",
+      storageBucket: "onlinedeliveryapp-19236.appspot.com",
+      messagingSenderId: "314147140443",
+      appId: "1:314147140443:web:a1ecbe9f3879bdf091607e",
       auth0Domain: "dev-ix8deo5de8ryya1n.us.auth0.com",
-      auth0ClientId: "U3WOrnybeZnBNvu4PrZSudWuHjkTMvai",
+      auth0ClientId: "U3WOrnybeZnBNvu4PrZSudWuHjkTMvai"
     },
   },
-}
+};
